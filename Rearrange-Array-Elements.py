3860b22ccbc082870b6ba518aa9aab2ca8bc5a47
Juan Morillo <morillox@hotmail.com>
2019-08-24 10:32:19 -0400

    File created

--- Rearrange-Array-Elements.py
+++ Rearrange-Array-Elements.py
@@ -1,26 +1,18 @@
 
 
 def rearrange_digits(input_list):
-    """
-    Rearrange Array Elements so as to form two number such that their sum is maximum.
-
-    Args:
-       input_list(list): Input List
-    Returns:
-       (int),(int): Two maximum sums
-    """
-    num1 = ""
-    num2 = ""
+    numONE = ""
+    numTWO = ""
     input_list = sorting_function(input_list)
 
     for index in range(0, len(input_list), 2):
-        num1 = str(input_list[index]) + num1
+        numONE = str(input_list[index]) + numONE
         try:
-            num2 = str(input_list[index+1]) + num2
+            numTWO = str(input_list[index+1]) + numTWO
         except Exception:
-            return [int(num1), int(num2)]
+            return [int(numONE), int(numTWO)]
 
-    return [int(num1), int(num2)]
+    return [int(numONE), int(numTWO)]
 
 
 def sorting_function(array):
@@ -34,20 +26,21 @@ def sorting_function(array):
 
 def merge_function(left, right):
     result = []
-    left_p = right_p = 0
+    leftPO = rightPO = 0
 
-    while left_p < len(left) and right_p < len(right):
-        if left[left_p] < right[right_p]:
-            result.append(left[left_p])
-            left_p += 1
+    while leftPO < len(left) and rightPO < len(right):
+        if left[leftPO] < right[rightPO]:
+            result.append(left[leftPO])
+            leftPO += 1
         else:
-            result.append(right[right_p])
-            right_p += 1
+            result.append(right[rightPO])
+            rightPO += 1
 
-    result.extend(left[left_p:])
-    result.extend(right[right_p:])
+    result.extend(left[leftPO:])
+    result.extend(right[rightPO:])
 
     return result
+
 
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
@@ -60,4 +53,3 @@ def test_function(test_case):
 
 test_function([[1, 2, 3, 4, 5], [542, 31]])
 test_function([[2, 4, 3, 1, 5], [542, 31]])
-test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
